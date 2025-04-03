c4edc42c21b630238c0cbafb4c5352ff60fcc659
Israel Santos <ysraellsanosss@gmail.com>
2025-04-02 22:23:47 -0300

    atualização, pois coloquei os aquivos errados

---
+++ classrom/src/components/button.tsx
@@ -0,0 +1,27 @@
+export function Button() {
+   return (
+      <div>
+         <h1>Primeiros comando com react</h1>
+         <main>
+
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+               Eos iste ea libero consequatur officiis consequuntur alias! Sequi
+               eius assumenda sed cum distinctio, labore, hic rem earum a eos modi deleniti.rem</p>
+
+            <div>
+               <input type="text" placeholder="Digite seu nome" />
+               <input type="text" placeholder="Digite seu email" />
+               <button>Clique aqui</button>
+            </div>
+
+            <div>
+            </div>
+
+         </main>
+
+
+      </div>
+
+
+   )
+}
